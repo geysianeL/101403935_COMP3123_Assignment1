82b658cae3127f64c4798a0bbb9b17264c528f8f
geysiane lopes <geysianelopes@hotmail.com>
2024-10-12 18:10:55 -0400

    add debug

--- src/app.js
+++ src/app.js
@@ -10,12 +10,12 @@ app.use('/api', userRoutes);
 app.use('/api', employeeRoutes);
 
 const port = 3000;
-const mongoUri = process.env.MONGO_URI;
+const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/comp3123Employees";
 
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUri)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
-  
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
