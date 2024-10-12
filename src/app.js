81db358eab5c64cdcb6f19a4bd682a5c1a5be914
geysiane lopes <geysianelopes@hotmail.com>
2024-10-12 13:33:46 -0400

    add mongodb and login create user

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 const employeeRoutes = require('./routes/employeeRoutes');
@@ -9,6 +10,12 @@ app.use('/api', userRoutes);
 app.use('/api', employeeRoutes);
 
 const port = 3000;
+const mongoUri = process.env.MONGO_URI;
+
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+  
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,30 @@
-exports.signUp = (req, res) => {
-    res.send('Get all users');
-  };
-  
-  exports.login = (req, res) => {
-    res.send('Create a new user');
-  };
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.signUp = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.status(201).send('User created successfully');
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).send('Invalid email or password');
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).send('Invalid email or password');
+
+    const token = jwt.sign({ id: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
